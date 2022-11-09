99c9b04fd3b7058438f0e74d8859c1ac2f6ea063
Aleksandrs Kunicins <tiv51tiv51@gmail.com>
2022-11-09 23:54:45 +0200

    test

--- zetoni.py
+++ zetoni.py
@@ -10,24 +10,10 @@ N = int(input("N = "))
 M1=int(input("M1 = "))
 M2=int(input("M2 = "))
 step=int(input("G = "))
-# def user1():
-#     usera=int(input("a = "))
-#     return usera
-# usera=user1()
-# def user2():
-#     userb=int(input("b = "))
-#     return userb
-# userb=user2()
-
-# user1a = int(input("a = "))
-# user1b = int(input("b = "))
 for cheack in range(1,N+1):
     if cheack%M1==0 or cheack%M2==0:
         mylist.append(cheack)
         labs+=1
-    # else:
-    #  print("Vērtības ir vairāk, neka žitonu skaits")
-
 while step>count:
     usera=int(input("a = "))
     userb=int(input("b = "))
@@ -38,4 +24,3 @@ while step>count:
     print(mylist)
     print(f"Atlikusi",labs," žetonu")
     count+=1
-#def
